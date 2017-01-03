141e74d71fccb6f8295d8e383e9e37190e5bae54
whstone_chen <chenzheqi.czq@163.com>
2017-01-03 17:38:06 +0800

    搭建electron+webpack

---
+++ app/index.js
@@ -0,0 +1,8 @@
+import 'babel-polyfill'; // generators
+import React from 'react';
+import { render } from 'react-dom';
+
+
+render(
+  <div>This is a div!</div>,
+  document.getElementById('example'))
---
+++ webpack.config.base.js
@@ -0,0 +1,29 @@
+
+import path from 'path'
+import validate from 'webpack-validator'
+
+export default validate({
+	module: {
+		loaders: [{
+			test: /\.jsx?$/,
+			loaders: ['babel-loader'],
+			exclude: /node_modules/
+		}]
+	},
+
+	output: {
+		path: path.join(__dirname, 'app'),
+		filename: 'bundle.js',
+
+		libraryTarget: 'commonjs2'
+	},
+
+	resolve: {
+		extensions: ['', '.js', '.jsx'],
+		packageMains: ['webpack', 'browser', 'web', 'browserify', ['jam', 'main'], 'main']
+	},
+
+	plugins: [],
+
+	
+})
