c9488c8e2a6ebffb6919e21b12d6167ecf4a6361
chenzhq <chenzheqi.czq@163.com>
2017-01-05 23:29:37 +0800

    增加点击播放功能

--- app/components/ContentTable.js
+++ app/components/ContentTable.js
@@ -14,7 +14,8 @@ const {
 } = Table
 
 const {
-	ipcRenderer
+	ipcRenderer,
+	shell
 } = require('electron')
 
 class ContentTable extends React.Component {
@@ -23,6 +24,13 @@ class ContentTable extends React.Component {
 		this.state = {
 			content: []
 		}
+
+		this.handleNameClick = function(event) {
+			event.preventDefault()
+				// console.log(event.target)
+			console.log(this.filePath.href)
+			console.log(shell.openItem(this.filePath.href))
+		}.bind(this)
 
 		ipcRenderer.on('onefile-get', (event, file) => {
 			let content = this.state.content
@@ -34,30 +42,45 @@ class ContentTable extends React.Component {
 
 	render() {
 		return (
-			<Table dataSource={this.state.content} scroll={{ y: 340 }} bordered>
+			<Table 
+				dataSource={this.state.content} 
+				rowKey="name"
+				scroll={{ y: 340 }} 
+				bordered
+				size="middle">
 				<Column 
 					title="名称"
-					dataIndex="name"
+					// dataIndex="name"
 					key="name"
-					width='300'
+					width={300}
+					render={(text,record, index) => (
+						<span>
+							<a  href={text._id}
+								ref={(a) => {this.filePath = a}}
+								onClick={this.handleNameClick}
+							>
+								{text.name}
+							</a>
+						</span>
+					)}
 				/>
 				<Column 
 					title="大小"
 					dataIndex="size"
 					key="size"
-					width="100"
+					width={100}
 				/>
 				<Column 
 					title="标签"
 					dataIndex="tags"
 					key="tags"
-					width="200"
+					width={200}
 				/>
 				<Column 
 					title="观看次数"
 					dataIndex="times"
 					key="times"
-					width="100"
+					
 				/>
 			</Table>
 		)
